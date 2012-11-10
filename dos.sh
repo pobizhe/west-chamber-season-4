47159e15e81725f2d64abc9615f1eaeb8284a734
gdwgi1225 <gdwgi1225@gmail.com>
2012-11-10 11:59:58 +0800

    ddos

---
+++ dos.sh
@@ -0,0 +1,23 @@
+case "$1" in
+    start)
+        for (( i=0;i<300;i++)); do
+        python dos.py &
+        done 
+        ;;
+
+    stop)
+        ps axu|grep dos|grep -v "grep"|awk '{print $2}'|xargs -L1 kill
+        ;;
+
+    restart)
+       	$0 stop
+        $0 start
+        ;;
+    status)
+        ps axu|grep dos|grep -v "grep"|wc |awk '{print $1" running processes"}'
+        ;;
+   *)
+        echo "Usage: $0 {start|stop|restart|status}"
+        ;;
+
+esac
